92970c95e654b5cf3f7a9bf7c1b982f00704c088
DESKTOP-4PBISAO\Anudeep <anudeepramadugu@gmail.com>
2025-04-29 20:05:09 +0530

    vfx manager instantiated

---
+++ Brotato Clone/Assets/Scripts/Data/VFX/VFXData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace BrotatoClone.Data
+{
+    [CreateAssetMenu(menuName = "Game Data/ VFX Data", fileName = "VFX Data")]
+    public class VFXData : ScriptableObject
+    {
+
+    }
+}
---
+++ Brotato Clone/Assets/Scripts/VFX/Manager/VFXManager.cs
@@ -0,0 +1,41 @@
+using BrotatoClone.Common;
+using BrotatoClone.Data;
+using BrotatoClone.Event;
+using BrotatoClone.Weapon;
+using UnityEngine;
+
+namespace BrotatoClone.VFX
+{
+    public class VFXManager : MonoBehaviour, IManager
+    {
+        [SerializeField] private VFXData vfxData;
+
+        private IEventManager eventManager;
+
+        public void InitializeManager(IEventManager eventManager)
+        {
+            SetManagerDependencies(eventManager);
+            RegisterEventListeners();
+            CreateControllers();
+        }
+
+        private void SetManagerDependencies(IEventManager eventManager)
+        {
+            this.eventManager = eventManager;
+        }
+
+        private void RegisterEventListeners()
+        {
+
+        }
+
+        private void CreateControllers()
+        {
+            
+        }
+
+        private void DisposeControllers()
+        {
+        }
+    }
+}
